b3af286c03c62c6cc204dded194193ddfa90dcfc
Abhishek Bohora <abhishek.bohora16@gmail.com>
2023-02-06 15:25:22 +0545

    Simple CRUD operations added

--- Controllers/DriversController.cs
+++ Controllers/DriversController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MapperApp.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MapperApp.Controllers
@@ -8,5 +9,63 @@ namespace MapperApp.Controllers
     public class DriversController : ControllerBase
     {
         private readonly ILogger<DriversController> _logger;
+        private static List<Driver> drivers = new List<Driver>();
+
+        [HttpGet]
+        public IActionResult GetDrivers()
+        {
+            var allDrivers = drivers.Where(x => x.Status == 1).ToList();
+            return Ok(allDrivers);
+        }
+
+        [HttpPost]
+        public IActionResult CreateDriver(Driver data)
+        {
+            if (ModelState.IsValid)
+            {
+                drivers.Add(data);
+                return CreatedAtAction("GetDriver", new { data.Id }, data);
+            }
+            return new JsonResult("Something went wrong") { StatusCode = 500 };
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetDriver(Guid id)
+        {
+            var driver = drivers.FirstOrDefault(x => x.Id == id);
+            if (driver == null) return NotFound();
+            return Ok(driver);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateDriver(Guid id, Driver data) {
+            if (id == data.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingDriver = drivers.FirstOrDefault(x => x.Id == data.Id);
+            if (existingDriver == null) {
+                return NotFound();
+            }
+
+            existingDriver.DriverNumber = data.DriverNumber;
+            existingDriver.FirstName = data.FirstName;
+            existingDriver.LastName = data.LastName;
+            existingDriver.WorldChampionships = data.WorldChampionships;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteDriver(Guid id)
+        {
+            var existingDriver = drivers.FirstOrDefault(x =>x.Id == id);
+            if (existingDriver == null)
+            {
+                return NotFound();
+            }
+            existingDriver.Status = 0;
+            return NoContent();
+        }
     }
 }
